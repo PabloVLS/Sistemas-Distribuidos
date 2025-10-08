a82081210ed12b5526e3ecb0a3c6298978d40ab0
Pablo Vinicius <128602954+PabloVLS@users.noreply.github.com>
2025-10-08 02:45:36 -0300

    Add files via upload

--- prj_veiculo/prj_veiculo/src/main/java/com/sd/prj_veiculo/prj_veiculo_service/VeiculoService.java
+++ prj_veiculo/prj_veiculo/src/main/java/com/sd/prj_veiculo/prj_veiculo_service/VeiculoService.java
@@ -2,7 +2,7 @@ package com.sd.prj_veiculo.prj_veiculo_service;
 
 import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.sd.prj_veiculo.prj_veiculo_model.Veiculo;
@@ -16,6 +16,7 @@ public class VeiculoService {
 
     private final VeiculoRepository veiculoRepository;
 
+    @Autowired
     public VeiculoService(VeiculoRepository veiculoRepository) {
         this.veiculoRepository = veiculoRepository;
     }
